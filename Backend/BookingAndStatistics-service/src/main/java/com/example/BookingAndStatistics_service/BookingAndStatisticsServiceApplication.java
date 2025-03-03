32a932641ceb62dab4242953eaa8e80c336c0321
Safwene17 <safwenenacheb17@gmail.com>
2025-03-03 14:47:48 +0100

    added eureuka server and client config for service-registry ,auth and notification services

--- Backend/BookingAndStatistics-service/src/main/java/com/example/BookingAndStatistics_service/BookingAndStatisticsServiceApplication.java
+++ Backend/BookingAndStatistics-service/src/main/java/com/example/BookingAndStatistics_service/BookingAndStatisticsServiceApplication.java
@@ -1,15 +1,13 @@
-package com.example.config_server;
+package com.example.BookingAndStatistics_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-
 @SpringBootApplication
-public class ConfigServerApplication {
+public class BookingAndStatisticsServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
+		SpringApplication.run(BookingAndStatisticsServiceApplication.class, args);
 	}
 
 }
